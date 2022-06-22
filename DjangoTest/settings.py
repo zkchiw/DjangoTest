98059f47258181740daafaa8b7a4a2cea8d8f49b
zkchiw <zkchiw@naver.com>
2022-06-22 16:15:13 +0900

    mariadb 연동

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'DjangoTest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': BASE_DIR / 'db.sqlite3', },
+'mariadb': {
+	'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'bigdata',
+	'USER': 'admin',
+	'PASSWORD': '',
+	'HOST': '????', }
 }
 
 
